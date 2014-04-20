3805fec89337fcfcd58b1afdb95c8b5ab4830288
Phillip Carter <phillip-carter@jmstudios.net>
2014-04-19 17:42:59 -0700

    Delayed reminders finished(maybe?)

--- app/src/main/java/com/jmstudios/corvallistransit/RouteAdapter.java
+++ app/src/main/java/com/jmstudios/corvallistransit/RouteAdapter.java
@@ -2,13 +2,20 @@ package com.jmstudios.corvallistransit;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v4.app.NotificationCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
-import android.widget.Toast;
+import android.os.Vibrator;
 
 import com.jmstudios.corvallistransit.models.Stop;
 
@@ -20,6 +27,7 @@ import java.util.List;
 public class RouteAdapter extends ArrayAdapter<Stop> {
     private final Activity mContext;
     private final List<Stop> mStops;
+    private static final int milisecondMultiplier = 60000;
 
     public RouteAdapter(Activity context, List<Stop> stops) {
         super(context, R.layout.fragment_main, stops);
@@ -70,7 +78,7 @@ public class RouteAdapter extends ArrayAdapter<Stop> {
         builder.setItems(R.array.timer_options,
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        Toast.makeText(mContext, "HEY MAN", Toast.LENGTH_SHORT).show();
+                        doNotificationBusiness(id);
                         dialog.cancel();
                     }
                 }
@@ -81,6 +89,83 @@ public class RouteAdapter extends ArrayAdapter<Stop> {
         AlertDialog alert = builder.create();
         alert.setCanceledOnTouchOutside(true);
         alert.show();
+    }
+
+    private void doNotificationBusiness(int id) {
+        int delay;
+
+        switch (id) {
+            case 0:
+                delay = milisecondMultiplier / 6;
+                break;
+            case 1:
+                delay = 5 * milisecondMultiplier;
+                break;
+            case 2:
+                delay = 10 * milisecondMultiplier;
+                break;
+            case 3:
+                delay = 15 * milisecondMultiplier;
+                break;
+            default:
+                delay = 0;
+                break;
+        }
+
+        final Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                buildNotification();
+                doVibrate();
+            }
+        }, delay);
+    }
+
+    private void doVibrate() {
+        // Get instance of Vibrator from current Context
+        Vibrator v = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
+
+        long[] pattern = { 0, 400, 200, 400, 200, 400 };
+
+        // -1 as the second parameter allows it to follow the pattern once
+        v.vibrate(pattern, -1);
+    }
+
+    private void buildNotification() {
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(mContext)
+                        .setSmallIcon(R.drawable.ic_launcher)
+                        .setContentTitle("Wheres My Bus")
+                        .setContentText("Get to your bus stop!");
+
+        // Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(mContext, MainActivity.class);
+
+        // The stack builder object will contain an artificial back stack for the
+        // started Activity.
+        // This ensures that navigating backward from the Activity leads out of
+        // your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(mContext);
+
+        // Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(MainActivity.class);
+
+        // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // mId allows you to update the notification later on.
+        mNotificationManager.notify(0, mBuilder.build());
     }
 
     static class ViewHolder {
