be0771500e3983838742443749a7196602e468d0
-mxb- <mxb.atria@gmail.com>
2014-04-29 17:25:27 -0700

    Changed NavBar to be opaque, and with bars

--- app/src/main/java/com/jmstudios/corvallistransit/utils/Utils.java
+++ app/src/main/java/com/jmstudios/corvallistransit/utils/Utils.java
@@ -48,12 +48,13 @@ public class Utils {
     }
 
     public static List<Stop> filterTimes(List<Stop> stops) {
-        for (Iterator<Stop> iterator = stops.iterator(); iterator.hasNext(); ) {
-            Stop s = iterator.next();
-            if (s.eta() < 1 || s.eta() > 30) {
-                iterator.remove();
+        if(stops!=null)
+            for (Iterator<Stop> iterator = stops.iterator(); iterator.hasNext(); ) {
+                Stop s = iterator.next();
+                if (s.eta() < 1 || s.eta() > 30) {
+                    iterator.remove();
+                }
             }
-        }
 
         return stops;
     }
