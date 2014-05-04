46ddf1e7daac3fb4195e621c36b7815daf867ea0
Phillip Carter <phillip-carter@jmstudios.net>
2014-05-04 15:58:26 -0700

    Adding onDestroy Override for ze map

--- app/src/main/java/com/jmstudios/corvallistransit/fragments/RouteMapFragment.java
+++ app/src/main/java/com/jmstudios/corvallistransit/fragments/RouteMapFragment.java
@@ -1,6 +1,7 @@
 package com.jmstudios.corvallistransit.fragments;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,9 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.jmstudios.corvallistransit.R;
@@ -21,8 +20,7 @@ import java.util.List;
 
 public class RouteMapFragment extends Fragment {
     private static final String ROUTE_IDX = "route_index";
-    private final double corvallisLat = 44.557285;
-    private final double corvallisLong = -123.2852531;
+    private final LatLng CORVALLIS = new LatLng(44.557285, -123.2852531);
     private GoogleMap map;
     private Route route;
 
@@ -38,6 +36,7 @@ public class RouteMapFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
 
+
         final View theView = inflater.inflate(R.layout.fragment_route_map, container, false);
 
         route = getRoute();
@@ -47,15 +46,21 @@ public class RouteMapFragment extends Fragment {
         return theView;
     }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+        FragmentManager fm = getFragmentManager();
+        MapFragment mf = (MapFragment) fm.findFragmentById(R.id.map);
+
+        if (mf != null) {
+            fm.beginTransaction().remove(mf).commit();
+        }
+    }
+
     private void initMap() {
         if (map == null) {
-            CameraPosition cameraPosition = new CameraPosition.Builder().target(
-                    new LatLng(corvallisLat, corvallisLong)).zoom(12).build();
-
-            GoogleMapOptions options = new GoogleMapOptions();
-            options.camera(cameraPosition);
-
-            MapFragment mapFragment = MapFragment.newInstance(options);
+            MapFragment mapFragment = MapFragment.newInstance();
             map = mapFragment.getMap();
 
             if (map != null) {
