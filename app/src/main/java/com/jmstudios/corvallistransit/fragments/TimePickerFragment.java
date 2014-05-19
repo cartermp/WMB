73a628996e7cafa300de5c56fa8acbe06e5bc67e
-mxb- <mxb.atria@gmail.com>
2014-05-18 19:24:38 -0700

    changed some stuff in the time picker

--- app/src/main/java/com/jmstudios/corvallistransit/fragments/TimePickerFragment.java
+++ app/src/main/java/com/jmstudios/corvallistransit/fragments/TimePickerFragment.java
@@ -4,9 +4,7 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
-import android.text.format.DateFormat;
 import android.widget.TimePicker;
-import java.util.Calendar;
 
 /**
  * Created by Bfriedman on 5/13/14.
